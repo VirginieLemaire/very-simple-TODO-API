73d9adfa69134a95b72fe74a53aa0c83012482ff
Virginie LEMAIRE <virginielemaire@free.fr>
2023-01-28 16:41:39 +0100

    router en place

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 
 const express = require('express');
+const router = require('./app/router');
+
 const app = express();
+
+app.use(router);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on http://localhost:${process.env.PORT}`);
